b93da6827f0880cf72ce3b63fe308996689d9f55
Efthymios Dellis <edellis@indeavor.com>
2023-05-30 21:20:11 +0300

    nginx on the play

--- azure/terraform/outputs.tf
+++ azure/terraform/outputs.tf
@@ -2,7 +2,3 @@ output "kube_config" {
     value=azurerm_kubernetes_cluster.myAKSCluster.kube_config_raw 
     sensitive = true
 }
-
-output "appligation_gateway_id" {
-  value =azurerm_application_gateway.myAG.id
-}
